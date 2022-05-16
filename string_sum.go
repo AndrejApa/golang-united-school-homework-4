c1ef22316999a66504e3623d21ab388f77c393f7
AndrejApa <81564427+AndrejApa@users.noreply.github.com>
2022-05-16 17:56:58 +0300

    "done"

--- string_sum.go
+++ string_sum.go
@@ -2,26 +2,38 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
+	errorEmptyInput     = errors.New("input is empty")
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
-
 func StringSum(input string) (output string, err error) {
-	return "", nil
+
+	input = strings.Join(strings.Fields(input), "")
+	if input == "" {
+		return "", fmt.Errorf("err: %w", errorEmptyInput)
+	}
+
+	var a = regexp.MustCompile(`([+-]?[^+-]+)`)
+	b := a.FindAllStringSubmatch(input, -1)
+	var x []int
+
+	for _, v := range b {
+		value, err := strconv.Atoi(v[0])
+		if err != nil {
+			return "", fmt.Errorf("err: %w", err)
+		}
+		x = append(x, value)
+	}
+
+	if len(x) != 2 {
+		return "", fmt.Errorf("err: %w", errorNotTwoOperands)
+	}
+	return strconv.Itoa(x[0] + x[1]), nil
 }
